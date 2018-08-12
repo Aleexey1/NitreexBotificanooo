b145c4ac172ac56c560972f9f0629b6b033b0c8e
Aleexeu <42278872+Aleexeu@users.noreply.github.com>
2018-08-12 13:40:53 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,6 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-    client.user.setActivity("Escreva aqui oque quer que apareça", {type: "PLAYING"});
     console.log('Loguei!');
 });
 
