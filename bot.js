49f29853674843a4e6c81a5d50f026d8770ef608
Aleexeu <42278872+Aleexeu@users.noreply.github.com>
2018-08-11 22:01:52 -0300

    Update and rename index.js to bot.js

---
+++ bot.js
@@ -0,0 +1,20 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+
+bot.on('message', message => {
+    if (message.content.startsWith('/canal')){
+        message.channel.send('Aqui está: https://www.youtube.com/channel/UCvCRTD0r6niKowGIUACV8NQ');
+    }
+    if (message.content.startsWith('/twitter')){
+        message.channel.send('Aqui está: https://twitter.com/AlexeyOficial');
+    } 
+    if (message.content.startsWith('/loja')){
+        message.channel.send('Aqui está: http://blastcraft.com.br/');
+    }
+    if (message.content.startsWith('/comandos')){
+        message.channel.send('Meus comandos são: /twitter - /canal - /loja - /comandos');
+    }
+});
+
+// THIS  MUST  BE  THIS  WAY
+client.login(process.env.BOT_TOKEN);
