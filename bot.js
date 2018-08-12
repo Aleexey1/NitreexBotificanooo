f4662f1d1c2b7ce6e5614f4670ba9f688c64d3c0
Aleexeu <42278872+Aleexeu@users.noreply.github.com>
2018-08-11 22:11:56 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,19 +1,17 @@
 const Discord = require('discord.js');
-const bot = new Discord.Client();
+const client = new Discord.Client();
 
-bot.on('message', message => {
-    if (message.content.startsWith('/canal')){
-        message.channel.send('Aqui está: https://www.youtube.com/channel/UCvCRTD0r6niKowGIUACV8NQ');
+client.on('ready', () => {
+    console.log('I am ready!');
+});
+
+client.on('message', message => {
+    if (message.content === 'ping') {
+    	message.reply('pong');
     }
-    if (message.content.startsWith('/twitter')){
-        message.channel.send('Aqui está: https://twitter.com/AlexeyOficial');
-    } 
-    if (message.content.startsWith('/loja')){
-        message.channel.send('Aqui está: http://blastcraft.com.br/');
-    }
-    if (message.content.startsWith('/comandos')){
-        message.channel.send('Meus comandos são: /twitter - /canal - /loja - /comandos');
-    }
+    if (message.content === '/pig'){
+        message.reply('pong');
+  	}
 });
 
 // THIS  MUST  BE  THIS  WAY
