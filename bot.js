1c8d400f4ed7166f90580ff1a73160663a11b6cd
Aleexeu <42278872+Aleexeu@users.noreply.github.com>
2018-08-12 13:12:33 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,8 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-    console.log('I am ready!');
+    bot.user.setActivity('meus comandos numa pasta.');
+    console.log('Loguei!');
 });
 
 client.on('message', message => {
