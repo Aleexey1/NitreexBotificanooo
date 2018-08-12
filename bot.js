2a1c2778d16ab213e9ba8c3d22dfc6b25b77e94c
Aleexeu <42278872+Aleexeu@users.noreply.github.com>
2018-08-11 22:18:40 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,6 +12,18 @@ client.on('message', message => {
     if (message.content === 'piga'){
         message.reply('pong');
   	}
+    if (message.content.startsWith('/canal')){
+        message.channel.send('Aqui está: https://www.youtube.com/channel/UCvCRTD0r6niKowGIUACV8NQ');
+    }
+    if (message.content.startsWith('/twitter')){
+        message.channel.send('Aqui está: https://twitter.com/AlexeyOficial');
+    } 
+    if (message.content.startsWith('/loja')){
+        message.channel.send('Aqui está: http://blastcraft.com.br/');
+    }
+    if (message.content.startsWith('/comandos')){
+        message.channel.send('Meus comandos são: /twitter - /canal - /loja - /comandos');
+    }
 });
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
