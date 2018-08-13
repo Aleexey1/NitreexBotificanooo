deea68f164937681cbfc85f78ad8df401037d1d6
Aleexeu <42278872+Aleexeu@users.noreply.github.com>
2018-08-12 22:48:45 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,13 +1,29 @@
-const  Discord  =  require ( ' discord.js ' );
- cliente  const =  novo  Discord.Client ();
- cliente . em ( ' pronto ' , () => {
-    console . log ( ' eu estou pronto! ' );
-});
- cliente . on ( ' mensagem ' , mensagem  => {
-    if ( mensagem . conteúdo  ===  ' Olá ' ) {
-    	mensagem . reply ( ' tudo bem? ' );
-  	}
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+    console.log('Loguei!');
 });
 
-// ESTE DEVE SER DESTA FORMA
-cliente . login ( processo . env . BOT_TOKEN );
+client.on('message', message => {
+    if (message.content === 'Oi') {
+    	message.reply('Olá, tudo bem ?');
+    }
+    if (message.content === 'piga'){
+        message.reply('pong');
+  	}
+    if (message.content.startsWith('/canal')){
+        message.channel.send('Aqui está: https://www.youtube.com/channel/UCvCRTD0r6niKowGIUACV8NQ');
+    }
+    if (message.content.startsWith('/twitter')){
+        message.channel.send('Aqui está: https://twitter.com/AlexeyOficial');
+    } 
+    if (message.content.startsWith('/loja')){
+        message.channel.send('Aqui está: http://blastcraft.com.br/');
+    }
+    if (message.content.startsWith('/comandos')){
+        message.channel.send('Meus comandos são: /twitter - /canal - /loja - /comandos');
+    }
+});
+// THIS  MUST  BE  THIS  WAY
+client.login(process.env.BOT_TOKEN);
