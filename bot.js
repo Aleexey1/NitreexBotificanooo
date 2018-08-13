6aed8a6843faac39336d64cbc56883ae13629b57
Aleexeu <42278872+Aleexeu@users.noreply.github.com>
2018-08-13 15:02:53 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ client.on('ready', () => {
 });
 
 client.on("guildMemberAdd", member => {
-    member.guild.channels.get('476793367334748176').send(member.user.username + ' entrou no sevrer!');
+    member.guild.channels.get('476108542202347555').send(member.user.username + ' entrou no sevrer!');
     member.send('Bem-Vindo ao servidor.');
 });
 
