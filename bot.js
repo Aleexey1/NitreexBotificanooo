707e7a07304d6eee8e2ae8fc3cb0fcde1f20a047
Aleexeu <42278872+Aleexeu@users.noreply.github.com>
2018-08-13 16:58:34 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-    client.user.setActivity('Merda');
+    bot.user.setActivity('Merda');
     console.log('logado');
 });
 
