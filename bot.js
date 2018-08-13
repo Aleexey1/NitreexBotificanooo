4feb12c4589fdf5f966b166f85190c7c36d7cad9
Aleexeu <42278872+Aleexeu@users.noreply.github.com>
2018-08-12 22:02:53 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,13 +2,8 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
+    bot.user.setActivity(`Escreva aqui oque quer que apareça`, {type: "PLAYING"});
     console.log('I am ready!');
-});
-
-client.on('message', message => {
-    if (message.content === 'Olá') {
-    	message.reply('tudo bem ?');
-  	}
 });
 
 // THIS  MUST  BE  THIS  WAY
