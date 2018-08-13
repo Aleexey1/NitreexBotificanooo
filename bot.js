0d86fc02a062504317e88fb8558c15c668a50f0e
Aleexeu <42278872+Aleexeu@users.noreply.github.com>
2018-08-13 17:24:24 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,27 +5,6 @@ client.on('ready', () => {
     console.log('logado');
 });
 
-export.run = (clientInformation,nessage,args)=>{
-    var razão = args.slice(1).join(" ")
-
-    var usuario = message.mentions.users.first();
-    if(!message.guild.member(message.author.id).hasPermissions("BAN_MEMBERS")) return message.replay("Você não tem permissão de usar esse comando!")
-    if(message.mentions.users.size < 1) return message.replay("Você não coloco a razão.")
-    if(!message.guild.member(usuario).bannable) return message.replay("Eu não posso banir essa pessoa.")
-    if(razão.length < 2) return message.replay("Você não colocou uma razão!")
-
-    message.guild.member(usuario).ban()
-
-    var discord = require ('discord.js')
-
-    var embed = new discord.RichEmbed()
-    .setTitle("Usuario banido do sever")
-    .addField("Usuario:",usuario.username)
-    .addField("razão:", razão);
-
-    message.channel.send(embed)
-}
-
 client.on("guildMemberAdd", member => {
     member.guild.channels.get('476793367334748176').send(member.user.username + ' entrou no sever!');
     member.send('Bem-Vindo ao servidor.');
