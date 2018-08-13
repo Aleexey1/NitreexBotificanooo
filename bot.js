dbe25097fde4bf1e1bd9e886d1697684712c8d5c
Aleexeu <42278872+Aleexeu@users.noreply.github.com>
2018-08-12 22:59:54 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -25,5 +25,6 @@ client.on('message', message => {
         message.channel.send('Meus comandos são: /twitter - /canal - /loja - /comandos');
     }
 });
+
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
