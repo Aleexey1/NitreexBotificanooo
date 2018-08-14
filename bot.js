7660fea06fae74c5d57588f358c3cb96761524b7
Aleexeu <42278872+Aleexeu@users.noreply.github.com>
2018-08-13 21:56:52 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,6 +2,11 @@ const Discord = require('discord.js');
 const bot = new Discord.Client();
 
 
+bot.on("guildMemberAdd", member => {
+    member.guild.channels.get('443601735244709918').send(member.user.username + ' entrou no sevrer!');
+    member.send('Bem-Vindo ao servidor.');
+});
+
 bot.on('ready', () => {
     console.log('Logado');
 });
