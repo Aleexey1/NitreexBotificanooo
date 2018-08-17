44562b1066e07a0a9b00b57f93c277290f7f1899
Aleexeu <42278872+Aleexeu@users.noreply.github.com>
2018-08-16 23:12:43 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -25,6 +25,9 @@ bot.on('message', message => {
     if (message.content.startsWith('/comandos')){
         message.channel.send('Meus comandos são: /twitter - /canal ');
     }
+    if(message.content.startsWith('tl') || message.channel.id !== "464215927710285825") {
+    message.reply('Comandos não são permitidos neste canal');
+    }
 });
 
 bot.on("ready", async () => {
